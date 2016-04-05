ac57549fd33f84aaf52e02428db3eba0e3ade49f
deseven <de7@deseven.info>
2016-04-05 15:29:16 +0200

    added dependency

--- esupload.rb
+++ esupload.rb
@@ -1,7 +1,9 @@
 class Esupload < Formula
   homepage 'https://github.com/deseven/esupload'
   desc 'a tool for OS X to upload files and screenshots via SSH'
-  head 'https://github.com/deseven/esupload.git'
+  url 'https://github.com/deseven/esupload.git', :branch => 'master'
+
+  depends_on 'terminal-notifier'
 
   def install
     bin.install 'esupload'
