dbc0dc5607633ce6aa789dcae43ae1f6ccbac072
deseven <de7@deseven.info>
2016-04-05 15:21:23 +0200

    capitalized class name

--- esupload.rb
+++ esupload.rb
@@ -1,4 +1,4 @@
-class esupload < Formula
+class Esupload < Formula
   homepage 'https://github.com/deseven/esupload'
   desc 'a tool for OS X to upload files and screenshots via SSH'
   head 'https://github.com/deseven/esupload.git'
